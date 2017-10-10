1f83a76fc1dd184ebb38baac7870a2d1da0c1ecd
Mario Meltzow <meltzow@invencom.de>
2017-10-10 16:31:33 +0200

    add EventSystem

---
+++ src/systems/EventSystem.ts
@@ -0,0 +1,38 @@
+
+export class EventBus {
+    static listeners: { [eventName: string]: ((event: Event) => void)[] };
+
+    static subscribe(cb: (event: Event) => void, event: Event) {
+        var foundListener = this.listeners[event.name];
+        if (!foundListener) {
+            foundListener = new Array();
+        }
+        let idx = foundListener.indexOf(cb);
+        if (idx < 0) {
+            foundListener.push(cb);
+        }
+    }
+
+    static unsubscribe(cb: (event: Event) => void, event: Event) {
+        var foundListener = this.listeners[event.name];
+        if (!foundListener) {
+            return;
+        }
+        let idx = foundListener.indexOf(cb);
+        foundListener.splice(idx, 1);
+    }
+
+    static post(event: Event) {
+        var foundListener = this.listeners[event.name];
+        if (!foundListener) {
+            return;
+        }
+        for (var idx in foundListener) {
+            foundListener[idx](event);
+        }
+    }
+}
+
+export abstract class Event {
+    public name: string;
+}
