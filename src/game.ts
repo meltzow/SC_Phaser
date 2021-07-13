a13ea834ca5880f7c12f0f6cf2e30a45705a0986
mario meltzow <dev@meltzow.net>
2021-07-13 19:18:53 +0200

    phaser 3 typescript example with isometric

--- src/game.ts
+++ src/game.ts
@@ -1,47 +1,256 @@
 import 'phaser';
 
-export default class Demo extends Phaser.Scene
+var directions = {
+    west: { offset: 0, x: -2, y: 0, opposite: 'east' },
+    northWest: { offset: 32, x: -2, y: -1, opposite: 'southEast' },
+    north: { offset: 64, x: 0, y: -2, opposite: 'south' },
+    northEast: { offset: 96, x: 2, y: -1, opposite: 'southWest' },
+    east: { offset: 128, x: 2, y: 0, opposite: 'west' },
+    southEast: { offset: 160, x: 2, y: 1, opposite: 'northWest' },
+    south: { offset: 192, x: 0, y: 2, opposite: 'north' },
+    southWest: { offset: 224, x: -2, y: 1, opposite: 'northEast' }
+};
+
+var anims = {
+    idle: {
+        startFrame: 0,
+        endFrame: 4,
+        speed: 0.2
+    },
+    walk: {
+        startFrame: 4,
+        endFrame: 12,
+        speed: 0.15
+    },
+    attack: {
+        startFrame: 12,
+        endFrame: 20,
+        speed: 0.11
+    },
+    die: {
+        startFrame: 20,
+        endFrame: 28,
+        speed: 0.2
+    },
+    shoot: {
+        startFrame: 28,
+        endFrame: 32,
+        speed: 0.1
+    }
+};
+
+
+var skeletons = [];
+
+var tileWidthHalf;
+var tileHeightHalf;
+
+var d = 0;
+
+var scene;
+
+// GameObject Skeleton
+class Skeleton extends Phaser.GameObjects.Image {
+    startX: number;
+    startY: number;
+    distance: any;
+    motion: any;
+    anim: any;
+    direction: any;
+    speed: number;
+    f: any;
+    constructor(scene, x, y, motion, direction, distance) {
+        super(scene, x, y, 'skeleton', direction.offset);
+
+        this.startX = x;
+        this.startY = y;
+        this.distance = distance;
+
+        this.motion = motion;
+        this.anim = anims[motion];
+        this.direction = directions[direction];
+        this.speed = 0.15;
+        this.f = this.anim.startFrame;
+
+        this.depth = y + 64;
+
+        scene.time.delayedCall(this.anim.speed * 1000, this.changeFrame, [], this);
+    }
+
+    changeFrame ()
+    {
+        this.f++;
+
+        var delay = this.anim.speed;
+
+        if (this.f === this.anim.endFrame)
+        {
+            switch (this.motion)
+            {
+                case 'walk':
+                    this.f = this.anim.startFrame;
+                    this.frame = this.texture.get(this.direction.offset + this.f);
+                    scene.time.delayedCall(delay * 1000, this.changeFrame, [], this);
+                    break;
+
+                case 'attack':
+                    delay = Math.random() * 2;
+                    scene.time.delayedCall(delay * 1000, this.resetAnimation, [], this);
+                    break;
+
+                case 'idle':
+                    delay = 0.5 + Math.random();
+                    scene.time.delayedCall(delay * 1000, this.resetAnimation, [], this);
+                    break;
+
+                case 'die':
+                    delay = 6 + Math.random() * 6;
+                    scene.time.delayedCall(delay * 1000, this.resetAnimation, [], this);
+                    break;
+            }
+        }
+        else
+        {
+            this.frame = this.texture.get(this.direction.offset + this.f);
+
+            scene.time.delayedCall(delay * 1000, this.changeFrame, [], this);
+        }
+    }
+
+    resetAnimation ()
+    {
+        this.f = this.anim.startFrame;
+
+        this.frame = this.texture.get(this.direction.offset + this.f);
+
+        scene.time.delayedCall(this.anim.speed * 1000, this.changeFrame, [], this);
+    }
+
+    update ()
+    {
+        if (this.motion === 'walk')
+        {
+            this.x += this.direction.x * this.speed;
+
+            if (this.direction.y !== 0)
+            {
+                this.y += this.direction.y * this.speed;
+                this.depth = this.y + 64;
+            }
+
+            //  Walked far enough?
+            if (Phaser.Math.Distance.Between(this.startX, this.startY, this.x, this.y) >= this.distance)
+            {
+                this.direction = directions[this.direction.opposite];
+                this.f = this.anim.startFrame;
+                this.frame = this.texture.get(this.direction.offset + this.f);
+                this.startX = this.x;
+                this.startY = this.y;
+            }
+        }
+    }
+}
+
+export default class Example extends Phaser.Scene
 {
     constructor ()
     {
-        super('demo');
+        super('');
     }
 
     preload ()
     {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
+        this.load.json('map', 'assets/iso/isometric-grass-and-water.json');
+        this.load.spritesheet('tiles', 'assets/iso/isometric-grass-and-water.png', { frameWidth: 64, frameHeight: 64 });
+        this.load.spritesheet('skeleton', 'assets/iso/skeleton8.png', { frameWidth: 128, frameHeight: 128 });
+        this.load.image('house', 'assets/iso/rem_0002.png');
     }
 
     create ()
     {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
+        scene = this;
 
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
+        this.buildMap();
+        this.placeHouses();
 
-        this.add.image(400, 300, 'libs');
+        skeletons.push(this.add.existing(new Skeleton(this, 240, 290, 'walk', 'southEast', 100)));
+        skeletons.push(this.add.existing(new Skeleton(this, 100, 380, 'walk', 'southEast', 230)));
+        skeletons.push(this.add.existing(new Skeleton(this, 620, 140, 'walk', 'south', 380)));
+        skeletons.push(this.add.existing(new Skeleton(this, 460, 180, 'idle', 'south', 0)));
 
-        const logo = this.add.image(400, 70, 'logo');
+    
 
-        this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
-            repeat: -1
-        })
+        this.cameras.main.setSize(1600, 600);
+
+        // this.cameras.main.scrollX = 800;
+    }
+
+    update ()
+    {
+        skeletons.forEach(function (skeleton) {
+            skeleton.update();
+        });
+
+      
+    }
+
+
+    buildMap ()
+    {
+        //  Parse the data out of the map
+        const data = scene.cache.json.get('map');
+
+        const tilewidth = data.tilewidth;
+        const tileheight = data.tileheight;
+
+        tileWidthHalf = tilewidth / 2;
+        tileHeightHalf = tileheight / 2;
+
+        const layer = data.layers[0].data;
+
+        const mapwidth = data.layers[0].width;
+        const mapheight = data.layers[0].height;
+
+        const centerX = mapwidth * tileWidthHalf;
+        const centerY = 16;
+
+        let i = 0;
+
+        for (let y = 0; y < mapheight; y++)
+        {
+            for (let x = 0; x < mapwidth; x++)
+            {
+                const id = layer[i] - 1;
+
+                const tx = (x - y) * tileWidthHalf;
+                const ty = (x + y) * tileHeightHalf;
+
+                const tile = scene.add.image(centerX + tx, centerY + ty, 'tiles', id);
+
+                tile.depth = centerY + ty;
+
+                i++;
+            }
+        }
+    }
+
+    placeHouses ()
+    {
+        const house_1 = scene.add.image(240, 370, 'house');
+        house_1.depth = house_1.y + 86;
+
+        const house_2 = scene.add.image(1300, 290, 'house');
+        house_2.depth = house_2.y + 86;
     }
 }
 
 const config = {
-    type: Phaser.AUTO,
-    backgroundColor: '#125555',
+    type: Phaser.WEBGL,
     width: 800,
     height: 600,
-    scene: Demo
+    backgroundColor: '#ababab',
+    parent: 'phaser-example',
+    scene: [ Example ]
 };
 
 const game = new Phaser.Game(config);
